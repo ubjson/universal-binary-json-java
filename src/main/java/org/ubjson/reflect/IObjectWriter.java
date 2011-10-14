44fe37387538e4bea3aa619eb9b857fd9eb879d6
Riyad Kalla <software@thebuzzmedia.com>
2011-10-14 12:06:47 -0700

    Added missing copywrite headers.

--- src/main/java/org/ubjson/reflect/IObjectWriter.java
+++ src/main/java/org/ubjson/reflect/IObjectWriter.java
@@ -1,3 +1,18 @@
+/**   
+ * Copyright 2011 The Buzz Media, LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ubjson.reflect;
 
 import java.io.IOException;
@@ -12,9 +27,11 @@ public interface IObjectWriter {
 	public void writeObject(UBJOutputStream out, Object obj)
 			throws IllegalArgumentException, IOException;
 
-	public void writeObject(UBJOutputStream out, Object obj, boolean autoCompact)
-			throws IllegalArgumentException, IOException;
+	public void writeObject(UBJOutputStream out, Object obj,
+			boolean useSmallestNumericStorage) throws IllegalArgumentException,
+			IOException;
 
 	public void writeObject(UBJOutputStream out, Object obj, AccessType type,
-			boolean compactNumericStorage) throws IllegalArgumentException, IOException;
+			boolean useSmallestNumericStorage) throws IllegalArgumentException,
+			IOException;
 }
