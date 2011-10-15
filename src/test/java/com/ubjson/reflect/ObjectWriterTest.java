3c7f9a3e93f2ce46695c694d99bb892c1df1619b
Riyad Kalla <software@thebuzzmedia.com>
2011-10-14 18:57:32 -0700

    Used for testing - outputs all the same test files used by the Marshallers but using the ObjectWriter for comparison.

---
+++ src/test/java/com/ubjson/reflect/ObjectWriterTest.java
@@ -0,0 +1,38 @@
+package com.ubjson.reflect;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.ubjson.io.UBJOutputStream;
+import org.ubjson.reflect.IObjectWriter;
+import org.ubjson.reflect.ObjectWriter;
+
+import com.ubjson.data.CouchDB4k;
+import com.ubjson.data.MediaContent;
+import com.ubjson.data.TwitterTimeline;
+
+public class ObjectWriterTest {
+	public static void main(String[] args) throws IOException {
+		IObjectWriter writer = new ObjectWriter();
+		UBJOutputStream out = new UBJOutputStream(new FileOutputStream(
+				"MediaContent-ObjectWriter.ubj"));
+
+		writer.writeObject(out, new MediaContent(), true);
+		out.flush();
+		out.close();
+
+		out = new UBJOutputStream(new FileOutputStream(
+				"CouchDB4k-ObjectWriter.ubj"));
+
+		writer.writeObject(out, new CouchDB4k(), true);
+		out.flush();
+		out.close();
+
+		out = new UBJOutputStream(new FileOutputStream(
+				"TwitterTimeline-ObjectWriter.ubj"));
+
+		writer.writeObject(out, new TwitterTimeline(), true);
+		out.flush();
+		out.close();
+	}
+}
