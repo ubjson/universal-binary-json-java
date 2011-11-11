06a9326d89603a0f6bf884812040aabc1357dfa5
Riyad Kalla <software@thebuzzmedia.com>
2011-11-10 21:53:29 -0700

    Refactored reflect package; it is a further abstraction of the IO libraries, it belongs under IO.

--- src/main/java/org/ubjson/io/reflect/ObjectWriter.java
+++ src/main/java/org/ubjson/io/reflect/ObjectWriter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.ubjson.reflect;
+package org.ubjson.io.reflect;
 
 import java.io.IOException;
 import java.lang.reflect.Array;
