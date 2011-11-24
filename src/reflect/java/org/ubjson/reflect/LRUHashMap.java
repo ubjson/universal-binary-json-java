2227e1acbb4529dd512772b7b92f5ffa6e82d559
Riyad Kalla <software@thebuzzmedia.com>
2011-11-24 09:12:37 -0700

    Moved LRUHashMap to reflect subpackage as this is the only code that will utilize it to cache member vars during reflection ops.

--- src/reflect/java/org/ubjson/reflect/LRUHashMap.java
+++ src/reflect/java/org/ubjson/reflect/LRUHashMap.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.ubjson.util;
+package org.ubjson.reflect;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
