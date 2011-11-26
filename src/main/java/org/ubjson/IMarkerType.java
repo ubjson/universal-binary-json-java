48fc2199c7ed4eb50d41803eb4741706438648fb
Riyad Kalla <software@thebuzzmedia.com>
2011-11-26 06:05:16 -0700

    Refactored root IO classes to core package.

--- src/main/java/org/ubjson/IMarkerType.java
+++ src/main/java/org/ubjson/IMarkerType.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.ubjson.io;
+package org.ubjson;
 
 public interface IMarkerType {
 	// General Types
