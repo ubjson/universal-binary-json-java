6b280028311264649b98bc8d087fdef4590c7bf9
Riyad Kalla <software@thebuzzmedia.com>
2012-02-07 14:58:46 -0700

    Much more optimized implementation of decode.
    ByteBuffer is no longer re-created on every single call; the single
    instance wrapping the byte[] buffer is used and re-used every single
    time.
    
    Caller must provide a CharBuffer big enough to hold the number of chars
    being decoded; previously this was being created inside of the decode
    method, but requiring the caller to send one (re-use) allows them to
    realize the requirements of this method and possibly cache/re-use
    buffers as many times as they want to save on the overhead.
    
    This method is *raw* decode logic now with next to no object creation
    and GC except what the underlying CharsetDecoder creates and uses.

--- src/main/java/org/ubjson/io/charset/StreamDecoder.java
+++ src/main/java/org/ubjson/io/charset/StreamDecoder.java
@@ -57,6 +57,7 @@ public class StreamDecoder {
 	}
 
 	private byte[] buffer;
+	private ByteBuffer bbuffer;
 	private CharsetDecoder decoder;
 
 	public StreamDecoder() {
@@ -67,81 +68,95 @@ public class StreamDecoder {
 		if (charset == null)
 			throw new IllegalArgumentException("charset cannot be null");
 
+		/*
+		 * We need access to both the byte[] and the wrapping ByteBuffer.
+		 * 
+		 * Below in decode() we need the raw byte[] to read in bytes from the
+		 * provided stream and we need access to the ByteBuffer to manipulate
+		 * its pointers to keep pointing it at the new data from the stream.
+		 */
 		buffer = new byte[BUFFER_SIZE];
+		bbuffer = ByteBuffer.wrap(buffer);
+
 		decoder = charset.newDecoder();
 	}
 
-	/*
-	 * TODO: Provide another decode() method that accepts a CharBuffer with a
-	 * capacity AT LEAST as big as length so the char buffer can be re-used for
-	 * further optimized performance.
-	 */
-	public CharBuffer decode(InputStream stream, int length)
+	public void decode(InputStream stream, int length, CharBuffer dest)
 			throws IllegalArgumentException, IOException {
 		if (stream == null)
 			throw new IllegalArgumentException("stream cannot be null");
 		if (length < 0)
 			throw new IllegalArgumentException("length [" + length
 					+ "] must be >= 0.");
+		if (dest == null)
+			throw new IllegalArgumentException(
+					"dest cannot be null and must be a CharBuffer with a large enough capacity to hold at most length ["
+							+ length + "] characters.");
+		if (length > dest.capacity())
+			throw new IllegalArgumentException(
+					"length ["
+							+ length
+							+ "] is larger than the capacity ["
+							+ dest.capacity()
+							+ "] of the given target CharBuffer; the given CharBuffer must be big enough to contain all the characters decoded from the given InputStream.");
 
 		// short-circuit
 		if (length == 0)
-			return CharBuffer.wrap(new char[0]);
-
-		/*
-		 * Create the read buffer.
-		 * 
-		 * The offset and length we pass doesn't matter because we actually have
-		 * to adjust it on each iteration of the loop below to make sure it is
-		 * "looking" at the bytes just read in from the stream into the
-		 * underlying array (buffer).
-		 */
-		ByteBuffer src = ByteBuffer.wrap(buffer, 0, 0);
-
-		/*
-		 * Create the buffer that we are going to decode into and return to the
-		 * caller.
-		 * 
-		 * Byte to Char conversion will *never* result in more chars than there
-		 * are bytes, so we know that the biggest possible buffer we will need
-		 * is length bytes long. If it ends up being less after doing the
-		 * decoding, the limit on the buffer will be truncated to indicate that.
-		 */
-		CharBuffer dest = CharBuffer.allocate(length);
-
-		// Reset the decoder to prepare it for new work.
-		decoder.reset();
-
-		int bytesRead = 0;
-
-		while (length > 0 && (bytesRead = stream.read(buffer, 0, length)) != -1) {
-			// Keep track of how many bytes remaining we need to read.
-			length -= bytesRead;
+			dest.clear();
+		else {
+			/*
+			 * Reset the ByteBuffer.
+			 * 
+			 * As we read actual bytes in from the given stream into the wrapped
+			 * byte[], we will adjust the ByteBuffers pointers to point at the
+			 * new data so the decode operation operates on the correct (new)
+			 * bytes.
+			 */
+			bbuffer.clear();
 
 			/*
-			 * Re-position the frame (position to limit) of the buffer to point
-			 * at the bytes just read in from the stream to the backing array.
+			 * Reset the dest CharBuffer.
+			 * 
+			 * This is the target we are decoding the bytes into. We have
+			 * confirmed its capacity is large enough to hold the maximum
+			 * possible number of chars we will decode (when 1-byte=1-char, e.g.
+			 * ASCII). Decoding will *never* result in more chars than bytes.
 			 */
-			src.position(0);
-			src.limit(bytesRead);
+			dest.clear();
 
-			// Decode the read bytes to our output buffer.
-			decoder.decode(src, dest, (length == 0));
+			// Reset the decoder to prepare it for new work.
+			decoder.reset();
+
+			int read = 0;
+
+			while (length > 0 && (read = stream.read(buffer, 0, length)) != -1) {
+				// Keep track of how many bytes remaining we need to read.
+				length -= read;
+
+				/*
+				 * Re-position the frame (position to limit) of the buffer to
+				 * point at the bytes just read in from the stream to the
+				 * backing array.
+				 */
+				bbuffer.position(0);
+				bbuffer.limit(read);
+
+				// Decode the read bytes to our output buffer.
+				decoder.decode(bbuffer, dest, (length == 0));
+			}
+
+			if (length > 0)
+				throw new IOException(
+						"End of Stream encountered before all requested bytes ["
+								+ (length + read)
+								+ "] could be read. Unable to read the last "
+								+ length + " remaining bytes.");
+
+			// Flush any remaining state from the decoder to our result.
+			decoder.flush(dest);
+
+			// Prepare the result buffer to be read by the caller.
+			dest.flip();
 		}
-
-		if (length > 0)
-			throw new IOException(
-					"End of Stream encountered before all requested bytes ["
-							+ (length + bytesRead)
-							+ "] could be read. Unable to read the last "
-							+ length + " remaining bytes.");
-
-		// Flush any remaining state from the decoder to our result.
-		decoder.flush(dest);
-
-		// Prepare the result buffer to be read by the caller.
-		dest.flip();
-
-		return dest;
 	}
 }
